a0a5b427c959bb93673424f652495a370fed5a01
William Wang <william.wang@edpsoftware.com>
2018-01-13 15:34:38 -0800

    adding map

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // get home page route
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Table for Two' });
 });
 
 router.get('/new', function(req, res) {
