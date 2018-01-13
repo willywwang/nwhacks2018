6a7de90dbb1b1bde845ad668ec94f2bfdeaf44cc
William Wang <william.wang@edpsoftware.com>
2018-01-13 14:47:01 -0800

    initial view commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap', 'chart.js', 'underscore']);
+
+app.run(['$rootScope', '$http', '$window', '$location',
+	function($rootScope, $http, $window, $location) {
+	}]);
+
+app.config(function($routeProvider, $locationProvider)	 {
+	$routeProvider.when('/', {
+		templateUrl: 'main.html',
+		controller: 'mainController'
+	})
+	.otherwise({redirectTo: '/'})
+
+	$locationProvider.html5Mode(true);
+});
