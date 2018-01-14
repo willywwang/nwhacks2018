2c108a5cab7e42a67106de637e5c607505c1f5f9
William Wang <william.wang@edpsoftware.com>
2018-01-13 19:01:42 -0800

    XD

--- public/javascripts/controllers/main.controller.js
+++ public/javascripts/controllers/main.controller.js
@@ -1,155 +1,170 @@
 angular.module('app')
 .controller('mainController', ['$scope', '$rootScope', '$http', '$window', '$uibModal',
 	function($scope, $rootScope, $http, $window, $uibModal) {
-		$scope.activeLink = 'restaurant';
+		$scope.initPage = function() {
+			$scope.activeLink = 'restaurant';
 
-		var item = {
-			coordinates: [49.2765, -123.2177]
-		};
-
-		var woa = {
-			city: 'This is my marker. There are many like it but this one is mine.'
-		};
-
-		var mapOptions = {
-			zoom: 17,
-			center: new google.maps.LatLng(49.2807513, -123.1152712),
-			mapTypeId: google.maps.MapTypeId.ROADMAP,
-			disableDefaultUI: true,
-			styles: [
-			{
-				"featureType": "administrative",
-				"elementType": "geometry",
-				"stylers": [
+			var mapOptions = {
+				zoom: 17,
+				center: new google.maps.LatLng(49.2807513, -123.1152712),
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				disableDefaultUI: true,
+				styles: [
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "administrative.land_parcel",
-				"stylers": [
+					"featureType": "administrative",
+					"elementType": "geometry",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "administrative.neighborhood",
-				"stylers": [
+					"featureType": "administrative.land_parcel",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "poi",
-				"stylers": [
+					"featureType": "administrative.neighborhood",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "poi",
-				"elementType": "labels.text",
-				"stylers": [
+					"featureType": "poi",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "poi.attraction",
-				"stylers": [
+					"featureType": "poi",
+					"elementType": "labels.text",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "poi.business",
-				"stylers": [
+					"featureType": "poi.attraction",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "poi.government",
-				"stylers": [
+					"featureType": "poi.business",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "poi.medical",
-				"stylers": [
+					"featureType": "poi.government",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "poi.park",
-				"stylers": [
+					"featureType": "poi.medical",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "poi.place_of_worship",
-				"stylers": [
+					"featureType": "poi.park",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "poi.school",
-				"stylers": [
+					"featureType": "poi.place_of_worship",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			},
-			{
-				"featureType": "poi.sports_complex",
-				"stylers": [
+					"featureType": "poi.school",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				},
 				{
-					"visibility": "off"
-				}
-				]
-			}]
-		};
+					"featureType": "poi.sports_complex",
+					"stylers": [
+					{
+						"visibility": "off"
+					}
+					]
+				}]
+			};
 
 
-		$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+			$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function (position) {
-				var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-				$scope.map.setCenter(initialLocation);
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(function (position) {
+					var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+					$scope.map.setCenter(initialLocation);
+				});
+			}
+
+			$scope.loadRestaurantMarkers();
+		}
+
+		$scope.loadRestaurantMarkers = function() {
+			var location = $scope.map.getCenter();
+			console.log(location.lat());
+			console.log(location.lng());
+			// http request here
+			$http({
+				method: 'GET',
+				url: 'https://developers.zomato.com/api/v2.1/search',
+				headers: {'user_key' : '39d7b0047f4d3176f7115db315b09012'},
+				params: {
+					lat: location.lat(),
+					lon: location.lng(),
+					radius: 200
+				}
+			}).then(function (res, err) {
+				console.log(res);
+				console.log(err);
+
+				$scope.mymarker = new google.maps.Marker({
+					map: $scope.map,
+					animation: google.maps.Animation.DROP,
+					position: $scope.map.getCenter(),
+					title: 'xd'
+				});
 			});
 		}
 
-    	//add marker
-    	$scope.addMarker = function(){
-    		$scope.mymarker = new google.maps.Marker({
-    			map: $scope.map,
-    			animation: google.maps.Animation.DROP,
-    			position: new google.maps.LatLng(item.coordinates[0], item.coordinates[1]),
-    			title: woa.city
-    		});
-    	};
+		$scope.initPage();
 
 		$scope.getActiveMenuLinkClass = function(path) {
 			if (!path) {
 				return '';
 			}
-			
+
 			return ($scope.activeLink === path) ? 'active' : '';
 		};
 
@@ -160,4 +175,4 @@ angular.module('app')
 		$scope.openPeopleList = function() {
 			$scope.activeLink = 'people';
 		};
-    }]);
+	}]);
