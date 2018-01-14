347c0f18705f5f69e3ca471b72f32dae6c60f450
William Wang <william.wang@edpsoftware.com>
2018-01-13 17:02:18 -0800

    adding new dependency

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
+app.use('/components', express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', index);
 app.use('/users', users);
